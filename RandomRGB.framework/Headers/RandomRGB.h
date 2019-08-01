6d5ba07753cb77660be1b78cd1c1933e195bcd37
Bhima Patange <bhimrao.p@phynart.com>
2019-08-01 14:04:20 +0530

    missing changes 2

---
+++ RandomRGB.framework/Headers/RandomRGB.h
@@ -0,0 +1,19 @@
+//
+//  RandomRGB.h
+//  RandomRGB
+//
+//  Created by Bhima on 01/08/19.
+//  Copyright © 2019 Bhima Patange. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RandomRGB.
+FOUNDATION_EXPORT double RandomRGBVersionNumber;
+
+//! Project version string for RandomRGB.
+FOUNDATION_EXPORT const unsigned char RandomRGBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RandomRGB/PublicHeader.h>
+
+
